e8c9ddd92569438e5939826c63c2d05515682b72
PointerRage <PointerRage@Dark-Messiah>
2017-03-26 13:21:13 +0300

    optimization eden cori ai: - fixed memory leak with test to collide entity/block (original minecraft methods save into global variable returning list with collide objects and memory (ArrayList inner object array) cannot be clean via GC; same reduced memory usage to big list with block & entity AABB objects - quicker algo to detect collide without big list builing

--- java/net/divinerpg/entities/twilight/EntityEdenCori.java
+++ java/net/divinerpg/entities/twilight/EntityEdenCori.java
@@ -1,178 +1,255 @@
 package net.divinerpg.entities.twilight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.divinerpg.entities.base.EntityDivineRPGFlying;
 import net.divinerpg.entities.twilight.projectile.EntityCoriShot;
 import net.divinerpg.libs.Sounds;
 import net.divinerpg.utils.items.TwilightItemsOther;
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 
 public class EntityEdenCori extends EntityDivineRPGFlying {
-	
-    public int courseChangeCooldown = 0;
-    public double waypointX;
-    public double waypointY;
-    public double waypointZ;
-    private Entity targetedEntity = null;
-    private int aggroCooldown = 0;
-    public int prevAttackCounter = 0;
-    public int attackCounter = 0;
-    private ChunkCoordinates currentFlightTarget;
-    
-    public EntityEdenCori(World var1) {
-        super(var1);
-    }
-    
-    @Override
-    protected void applyEntityAttributes() {
-        super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(net.divinerpg.entities.base.EntityStats.edenCoriHealth);
-        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(net.divinerpg.entities.base.EntityStats.edenCoriSpeed);
-        this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(net.divinerpg.entities.base.EntityStats.edenCoriFollowRange);
-    }
 
-    protected void entityInit() {
-        super.entityInit();
-        this.dataWatcher.addObject(16, Byte.valueOf((byte)0));
-    }
-    
-    @Override
-    public void onUpdate() {
-    	super.onUpdate();
-    	if(!this.worldObj.isRemote && this.worldObj.difficultySetting == EnumDifficulty.PEACEFUL) {
-            this.setDead();
-        }
+	private int courseChangeCooldown = 1;
+	private double waypointX = Double.NaN;
+	private double waypointY = Double.NaN;
+	private double waypointZ = Double.NaN;
+	private Entity targetedEntity = null;
+	private int aggroCooldown = 0;
+	private int attackCounter = 0;
+	private ChunkCoordinates currentFlightTarget;
 
-        this.despawnEntity();
-        this.prevAttackCounter = this.attackCounter;
-        double d0 = this.waypointX - this.posX;
-        double d1 = this.waypointY - this.posY;
-        double d2 = this.waypointZ - this.posZ;
-        double d3 = d0 * d0 + d1 * d1 + d2 * d2;
+	public EntityEdenCori(World world) {
+		super(world);
+	}
 
-        if(d3 < 1.0D || d3 > 3600.0D) {
-            this.waypointX = this.posX + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
-            this.waypointY = this.posY + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
-            this.waypointZ = this.posZ + (double)((this.rand.nextFloat() * 2.0F - 1.0F) * 16.0F);
-        }
+	@Override
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(net.divinerpg.entities.base.EntityStats.edenCoriHealth);
+		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(net.divinerpg.entities.base.EntityStats.edenCoriSpeed);
+		getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(net.divinerpg.entities.base.EntityStats.edenCoriFollowRange);
+	}
 
-        if(this.courseChangeCooldown-- <= 0) {
-            this.courseChangeCooldown += this.rand.nextInt(5) + 2;
-            d3 = (double)MathHelper.sqrt_double(d3);
+	@Override
+	protected void entityInit() {
+		super.entityInit();
+		dataWatcher.addObject(16, Byte.valueOf((byte) 0));
+	}
 
-            if(this.isCourseTraversable(this.waypointX, this.waypointY, this.waypointZ, d3)) {
-                this.motionX += d0 / d3 * 0.1D;
-                this.motionY += d1 / d3 * 0.1D;
-                this.motionZ += d2 / d3 * 0.1D;
-            } else {
-                this.waypointX = this.posX;
-                this.waypointY = this.posY;
-                this.waypointZ = this.posZ;
-            }
-        }
+	@Override
+	public void onUpdate() {
+		super.onUpdate();
+		if (!worldObj.isRemote && worldObj.difficultySetting == EnumDifficulty.PEACEFUL) {
+			setDead();
+		}
 
-        if(this.targetedEntity != null && this.targetedEntity.isDead) {
-            this.targetedEntity = null;
-        }
+		despawnEntity();
 
-        if(this.targetedEntity == null || this.aggroCooldown-- <= 0) {
-            this.targetedEntity = this.worldObj.getClosestVulnerablePlayerToEntity(this, 100.0D);
+		final double dx = waypointX - posX;
+		final double dy = waypointY - posY;
+		final double dz = waypointZ - posZ;
+		double distance = dx * dx + dy * dy + dz * dz;
 
-            if(this.targetedEntity != null) {
-                this.aggroCooldown = 20;
-            }
-        }
+		if (distance < 1.0D || distance > 3600.0D || distance == Double.NaN) {
+			waypointX = posX + (rand.nextFloat() * 2.0F - 1.0F) * 16.0F;
+			waypointY = posY + (rand.nextFloat() * 2.0F - 1.0F) * 16.0F;
+			waypointZ = posZ + (rand.nextFloat() * 2.0F - 1.0F) * 16.0F;
+		}
 
-        double d4 = 64.0D;
+		courseChangeCooldown--;
+		if (courseChangeCooldown == 0) {
+			courseChangeCooldown += rand.nextInt(5) + 2;
+			distance = MathHelper.sqrt_double(distance);
 
-        if(this.targetedEntity != null && this.targetedEntity.getDistanceSqToEntity(this) < d4 * d4) {
-            double d5 = this.targetedEntity.posX - this.posX;
-            double d6 = this.targetedEntity.boundingBox.minY+1 - this.posY;
-            double d7 = this.targetedEntity.posZ - this.posZ;
-            this.renderYawOffset = this.rotationYaw = -((float)Math.atan2(d5, d7)) * 180.0F / (float)Math.PI;
+			if (isCourseTraversable(dx, dy, dz, distance)) {
+				motionX += dx / distance * 0.1D;
+				motionY += dy / distance * 0.1D;
+				motionZ += dz / distance * 0.1D;
+			} else {
+				waypointX = posX;
+				waypointY = posY;
+				waypointZ = posZ;
+			}
+		}
 
-            if(this.canEntityBeSeen(this.targetedEntity)) {
-                ++this.attackCounter;
+		if (targetedEntity == null || targetedEntity.isDead || aggroCooldown-- <= 0) {
+			targetedEntity = worldObj.getClosestVulnerablePlayerToEntity(this, 100.0D);
 
-                if(this.attackCounter == 20) {
-            		this.worldObj.playSoundAtEntity(this.targetedEntity, Sounds.getSoundName(Sounds.coriShoot), 1.0F, 1.0F);
-            		EntityCoriShot shot = new EntityCoriShot(this.worldObj, this, 30);
-            		shot.setThrowableHeading(d5, d6, d7, 1.6f, 4);
-            		if(!this.worldObj.isRemote)this.worldObj.spawnEntityInWorld(shot);
-            		this.attackCounter = -40;
-                }
-            }
-            else if(this.attackCounter > 0) {
-                --this.attackCounter;
-            }
-        } else {
-            this.renderYawOffset = this.rotationYaw = -((float)Math.atan2(this.motionX, this.motionZ)) * 180.0F / (float)Math.PI;
+			if (targetedEntity != null) {
+				aggroCooldown = 20;
+			}
+		}
 
-            if(this.attackCounter > 0) {
-                --this.attackCounter;
-            }
-        }
+		if (targetedEntity != null && targetedEntity.getDistanceSqToEntity(this) < 64*64) {
+			double targetDx = targetedEntity.posX - posX;
+			double targetDy = targetedEntity.boundingBox.minY + 1 - posY;
+			double targetDz = targetedEntity.posZ - posZ;
+			renderYawOffset = rotationYaw = -((float) Math.atan2(targetDx, targetDz)) * 180.0F / (float) Math.PI;
 
-        if(!this.worldObj.isRemote) {
-            byte b1 = this.dataWatcher.getWatchableObjectByte(16);
-            byte b0 = (byte)(this.attackCounter > 10 ? 1 : 0);
+			if (canEntityBeSeen(targetedEntity)) {
+				attackCounter++;
 
-            if(b1 != b0) {
-                this.dataWatcher.updateObject(16, Byte.valueOf(b0));
-            }
-        }
-    }
+				if (attackCounter == 20) {
+					worldObj.playSoundAtEntity(targetedEntity, Sounds.getSoundName(Sounds.coriShoot), 1.0F, 1.0F);
+					EntityCoriShot shot = new EntityCoriShot(worldObj, this, 30);
+					shot.setThrowableHeading(targetDx, targetDy, targetDz, 1.6f, 4);
+					if (!worldObj.isRemote)
+						worldObj.spawnEntityInWorld(shot);
+					attackCounter = -40;
+				}
+			} else if (attackCounter > 0) {
+				attackCounter--;
+			}
+		} else {
+			renderYawOffset = rotationYaw = -((float) Math.atan2(motionX, motionZ)) * 180.0F / (float) Math.PI;
 
-    private boolean isCourseTraversable(double par1, double par3, double par5, double par7) {
-        double var9 = (this.waypointX - this.posX) / par7;
-        double var11 = (this.waypointY - this.posY) / par7;
-        double var13 = (this.waypointZ - this.posZ) / par7;
-        AxisAlignedBB var15 = this.boundingBox.copy();
+			if (attackCounter > 0) {
+				attackCounter--;
+			}
+		}
 
-        for(int var16 = 1; (double)var16 < par7; ++var16) {
-            var15.offset(var9, var11, var13);
+		if (!worldObj.isRemote) {
+			byte b1 = this.dataWatcher.getWatchableObjectByte(16);
+			byte b0 = (byte) (attackCounter > 10 ? 1 : 0);
 
-            if(!this.worldObj.getCollidingBoundingBoxes(this, var15).isEmpty()) {
-                return false;
-            }
-        }
+			if (b1 != b0) {
+				dataWatcher.updateObject(16, Byte.valueOf(b0));
+			}
+		}
+	}
 
-        return true;
-    }
+	// TODO n3k0: [performance] make AABB step by box size
+	private boolean isCourseTraversable(double x, double y, double z, double distance) {
+		final double nx = x / distance; // normal vector coords
+		final double ny = y / distance;
+		final double nz = z / distance;
 
-    @Override
-    protected String getLivingSound() {
-        return Sounds.getSoundName(Sounds.coriIdle);
-    }
+		final AxisAlignedBB aabb = boundingBox.copy();
+		for (int i = 1; i < distance; i++) {
+			aabb.offset(nx, ny, nz);
 
-    @Override
-    protected String getHurtSound() {
-        return Sounds.getSoundName(Sounds.coriHurt);
-    }
+			if (isBlockCollide(aabb) || isEntityCollide(aabb)) {
+				return false;
+			}
+		}
 
-    @Override
-    protected String getDeathSound() {
-        return Sounds.getSoundName(Sounds.coriHurt);
-    }
+		return true;
+	}
 
-    @Override
-    protected Item getDropItem() {
-        return TwilightItemsOther.edenSoul;
-    }
+	// TODO n3k0: [refactor] move this code part to WorldUtils as faster collide
+	// check
+	private boolean isBlockCollide(AxisAlignedBB aabb) {
+		final int minx = MathHelper.floor_double(aabb.minX);
+		final int maxx = MathHelper.floor_double(aabb.maxX + 1.0D);
+		final int miny = MathHelper.floor_double(aabb.minY);
+		final int maxy = MathHelper.floor_double(aabb.maxY + 1.0D);
+		final int minz = MathHelper.floor_double(aabb.minZ);
+		final int maxz = MathHelper.floor_double(aabb.maxZ + 1.0D);
+
+		final List collides = new ArrayList();
+		for (int x = minx; x < maxx; x++) {
+			for (int z = minz; z < maxz; z++) {
+				if (!worldObj.blockExists(x, 64, z)) {
+					continue;
+				}
+
+				for (int y = miny; y < maxy; y++) {
+					final Block block;
+					if (x >= -30000000 && x < 30000000 && z >= -30000000 && z < 30000000) {
+						block = worldObj.getBlock(x, y, z);
+					} else {
+						block = Blocks.stone;
+					}
+
+					block.addCollisionBoxesToList(worldObj, x, y, z, aabb, collides, this);
+					if (!collides.isEmpty()) {
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private boolean isEntityCollide(AxisAlignedBB aabb) {
+		final int minx = MathHelper.floor_double((aabb.minX - World.MAX_ENTITY_RADIUS) / 16.0D);
+		final int maxx = MathHelper.floor_double((aabb.maxX + World.MAX_ENTITY_RADIUS) / 16.0D);
+		final int miny = MathHelper.floor_double((aabb.minY - World.MAX_ENTITY_RADIUS) / 16.0D);
+		final int maxy = MathHelper.floor_double((aabb.maxY + World.MAX_ENTITY_RADIUS) / 16.0D);
+		final int minz = MathHelper.floor_double((aabb.minZ - World.MAX_ENTITY_RADIUS) / 16.0D);
+		final int maxz = MathHelper.floor_double((aabb.maxZ + World.MAX_ENTITY_RADIUS) / 16.0D);
+
+		final List collides = new ArrayList();
+		for (int x = minx; x <= maxx; x++) {
+			for (int z = minz; z <= maxz; z++) {
+				if (!worldObj.checkChunksExist(x, 0, z, x, 0, z)) {
+					continue;
+				}
+
+				final Chunk chunk = worldObj.getChunkFromChunkCoords(x, z);
+				final int chunkMinY = MathHelper.clamp_int(miny, 0, chunk.entityLists.length - 1);
+				final int chunkMaxY = MathHelper.clamp_int(maxy, 0, chunk.entityLists.length - 1);
+
+				for (int y = chunkMinY; y <= chunkMaxY; y++) {
+					final List entitys = chunk.entityLists[y];
+
+					for (int i = 0; i < entitys.size(); i++) {
+						final Entity entity = (Entity) entitys.get(i);
+						if (entity == this) {
+							continue;
+						}
+
+						if (entity.boundingBox.intersectsWith(aabb)) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	protected String getLivingSound() {
+		return Sounds.getSoundName(Sounds.coriIdle);
+	}
+
+	@Override
+	protected String getHurtSound() {
+		return Sounds.getSoundName(Sounds.coriHurt);
+	}
+
+	@Override
+	protected String getDeathSound() {
+		return Sounds.getSoundName(Sounds.coriHurt);
+	}
+
+	@Override
+	protected Item getDropItem() {
+		return TwilightItemsOther.edenSoul;
+	}
 
 	@Override
 	public String mobName() {
 		return "Weak Cori";
 	}
-	
+
+	@Override
 	public int getMaxSpawnedInChunk() {
-	    return 1;
-    }
+		return 1;
+	}
 }
